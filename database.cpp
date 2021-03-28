b5173ddb4e0a6124c83018c61153aba0d168976c
rinaryu <57197674+rinaryu@users.noreply.github.com>
2021-03-27 19:43:36 -0700

    Add files via upload

---
+++ database.cpp
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////
+
+// database.cpp
+
+#include "final_project.h"
+#include "cmpt_error.h"
+#include <iostream>
+
+// Definition
+class Database: public Person_info {
+    private:
+
+    public:
+    
+};
+
+#endif
