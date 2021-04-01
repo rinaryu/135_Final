780e8223cd0a9cf8eea3367c9eb293edaa28d3df
rinaryu <rina.ryuRR@gmail.com>
2021-04-01 11:17:52 -0700

    Person_info modifications
    changed new_person() method
    -- makes sure that the input for phone_number field is a number
    
    **also fixed some indentation throughout the file

--- Person_info.cpp
+++ Person_info.cpp
@@ -3,6 +3,7 @@
 //////////////////////////////////////////////////////////////////////////////////////
 //This class:
 // - gets user input to add new information into the database
+// - also makes sure that input is valid for all fields
 
 #include "database_head.h"
 #include "cmpt_error.h"
@@ -31,9 +32,9 @@ void Person_info::new_person (){
 	//call valid_name to check if user input is valid (is a valid name) 
 	if(!valid_name(new_name)){
 	    while(true){
-		cout<<"That is not a valid name, please provide a different name of alphabetical characters: \n";
-		getline(cin, new_name);
-		if(valid_name(new_name)) break; 
+			cout<<"That is not a valid name, please provide a different name of alphabetical characters: \n";
+			getline(cin, new_name);
+			if(valid_name(new_name)) break; 
 	    }              
 	}
 	//Second: Getting birthday of new person. 
@@ -41,14 +42,14 @@ void Person_info::new_person (){
 	getline(cin, dob);
 	while(true){
 	    if(!(dob.at(2) == '/') && !(dob.at(5) == '/')){
-		cout<<"The date of birth is not in the correct format, please re-enter: \n";
-		getline(cin, dob);
-		continue;
+			cout<<"The date of birth is not in the correct format, please re-enter: \n";
+			getline(cin, dob);
+			continue;
 	    }
 	    if(!valid_date(dob)){
-		cout<<"That is not a valid date of birth, please re-enter: \n";
-		getline(cin, dob);
-		if(valid_date(dob)) break;
+			cout<<"That is not a valid date of birth, please re-enter: \n";
+			getline(cin, dob);
+			if(valid_date(dob)) break;
 	    }
 	}
 	//Third: Getting the city that the new person resides in. 
@@ -61,33 +62,39 @@ void Person_info::new_person (){
 	cout<<"new_city: "<<new_city<<endl;
 	if(!valid_city(new_city)){
 	    while(true){
-		cout<<"That is not a valid city, please enter a city in the Lower Mainland: \n";
-		getline(cin, new_city);
-		//changing the user input to all lowercase letters
-		for(int i = 0; i < new_city.size(); i++){
-		  new_city.at(i) = towlower(new_city.at(i));
-		}
-		if(valid_city(new_city)) break;
+			cout<<"That is not a valid city, please enter a city in the Lower Mainland: \n";
+			getline(cin, new_city);
+			//changing the user input to all lowercase letters
+			for(int i = 0; i < new_city.size(); i++){
+				new_city.at(i) = towlower(new_city.at(i));
+			}
+			if(valid_city(new_city)) break;
 	    }
 	}
 	//Fourth: Getting phone number of new person. 
 	cout<<"Please enter their phone number: \n";
 	cin >> phone_num;
-	if(!valid_phone(phone_num)){
-	    while(true){
-		cout<<"This is not a valid phone number, please re-enter: \n";
-		cin >> phone_num;
-		if(valid_phone(phone_num)) break;
-	    }
+	while(true){
+		if(cin.fail()){
+			cout<<"This is not a number, please re-enter: \n";
+			cin.clear();
+			cin.ignore(10000, '\n');
+			cin >> phone_num;
+		}
+		if(valid_phone(phone_num)) break; 
+		else {
+			cout<<"This is not a valid phone number, please re-enter: \n";
+			cin >> phone_num;
+		}
 	}
-	//Fifth: Getting if new person is vaccinated or nots. 
+	//Fifth: Getting if new person is vaccinated or not. 
 	cout<<"Is the new person vaccinated? (y/n)";
 	getline(cin, vaccinated);
 	while(true){
 	    if(vaccinated == "y" || vaccinated == "n") break;
 	    else{
-		cout<<"That is not a valid answer: please enter y or n.\n";
-		getline(cin, vaccinated);
+			cout<<"That is not a valid answer: please enter y or n.\n";
+			getline(cin, vaccinated);
 	    }
 	}
 }
@@ -99,7 +106,7 @@ void Person_info::new_person (){
 bool Person_info::valid_name (string user_input){
     for (int i = 0; i < user_input.size(); i++){
 	    if (!isalpha(user_input.at(i))){
-		return 0;
+			return 0;
 	    } 
 	}
 	return 1;
@@ -110,20 +117,21 @@ bool Person_info::check_date (int month, int day, int year){
 
 	if (month == 2){
 	    if (year % 4 == 0){
-		if (day > 29 || day < 0) return 0;
+			if (day > 29 || day < 0) return 0;
 	    } else {
-		if (day > 28 || day < 0) return 0;
+			if (day > 28 || day < 0) return 0;
 	    } 
-	} else if (month == 1 || month == 3 || month == 5 ||
+	} 
+	else if (month == 1 || month == 3 || month == 5 ||
 		    month == 7 || month == 8 || month == 10 ||
-		    month == 12)
-	{
+		    month == 12){
 	    if (day > 31 || day < 0) return 0;
 	} else {
 	    if (day > 30 || day < 0) return 0;
 	}
 
 	if (year < 1940 || year > 2005) return 0;
+
 	return 1;
 }
 bool Person_info::valid_date (string user_input){
@@ -160,10 +168,8 @@ bool Person_info::valid_phone (int num){
 	string temp_num = to_string(num);
 	if(temp_num.size() == 7){
 	    for(string code : area_codes){
-		if(temp_num.substr(0,3) == code){
-		    return 1;
-		}
+			if(temp_num.substr(0,3) == code) return 1;
 	    }
 	} 
-		return 0;
+	return 0;
 }
