365c3cc922638248a23214673f30f93886a7b5ab
Lynn Nguyen <81499105+lynn9101@users.noreply.github.com>
2021-04-16 23:27:27 -0700

    Update final_main.cpp

--- final_main.cpp
+++ final_main.cpp
@@ -6,7 +6,48 @@ void final_test(){ //change to better function name later
 	bool done = false;
 	while(!done){
 		char x = feature_display();
+		//char res;#include "database_head.h"
+using namespace std;
+
+void final_test(){
+	Menu menu;
+	bool done = false;
+	while(!done){
+		char x = feature_display();
 		//char res;
+		
+		if(x == 'a'){
+			endwin();
+			menu.adding();
+			done = false;
+		}
+		
+		if(x == 's' || x == 'd'){
+			endwin();	
+			menu.searching(x);
+		}
+		
+		else if (x == 'u'){
+			endwin();
+			menu.update();
+		} 
+
+		if (x == 'l'){
+			endwin();
+			menu.listing();
+		}	
+		
+		if(x == 'q') {
+			endwin();
+		 	menu.quitting();
+		 	done = true;
+		}
+	}
+	endwin();
+}
+int main(){
+   	final_test();
+}
 		
 		if(x == 'a'){
 			endwin();
